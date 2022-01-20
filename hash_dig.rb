0f87f82f3488370408db52e65ca9f51891f0c033
walker-cook <jwcook99@gmail.com>
2022-01-20 18:41:25 +0000

    done

--- hash_dig.rb
+++ hash_dig.rb
@@ -1,13 +1,11 @@
 # Write a program that uses the sample_hash variable and prints the value of key "history"
 
 sample_hash = {
-   :class => { 
-      :student => { 
-         :name => "Mike",
-         "marks" => { 
-            "physics" => 70,
-            "history" => 80
+   :class => { :student => { :name => "Mike", "marks" => { "physics" => 70,"history" => 80
          }
       }
    }
 }
+
+p sample_hash.fetch(:class).fetch(:student).fetch("marks").fetch("history")
+
--- hash_find_value.rb
+++ hash_find_value.rb
@@ -7,3 +7,31 @@
 sample_hash = {:a => 100, :b => 200, :c => 300, :d => 400, :e => rand(200), :f => 600, :g => 0 }
 
 p "Enter an integer to find:"
+number_to_find = gets.chomp
+
+if sample_hash.fetch(:a).to_s == number_to_find.to_s
+  p number_to_find + " is under the key: a."
+elsif sample_hash.fetch(:b).to_s == number_to_find.to_s
+  p number_to_find + " is under the key: b."
+  
+elsif sample_hash.fetch(:c).to_s == number_to_find.to_s
+  p number_to_find + " is under the key: c."
+
+elsif sample_hash.fetch(:d).to_s == number_to_find.to_s
+  p number_to_find + " is under the key: d."
+
+elsif sample_hash.fetch(:e).to_s == number_to_find.to_s
+  p number_to_find + " is under the key: e."
+
+elsif sample_hash.fetch(:f).to_s == number_to_find.to_s
+  p number_to_find + " is under the key: f."
+
+elsif sample_hash.fetch(:g).to_s == number_to_find.to_s
+  p number_to_find + " is under the key: g."
+  
+else
+  p "Could not find the integer " + number_to_find
+  
+end
+  
+
